dbe836008b2d8c7e7416b40b8838f87a3bf9391f
mcbontempi <mcbontempi@gmail.com>
2013-08-15 19:48:49 +0100

    new renamed project added

---
+++ DelegatePatternUsingProtocol/DelegatePatternUsingProtocol/DDTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDTAppDelegate.h
+//  DelegatePatternUsingProtocol
+//
+//  Created by Daren David Taylor on 15/08/2013.
+//  Copyright (c) 2013 DarenDavidTaylor.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DelegatePatternUsingProtocol/DelegatePatternUsingProtocolTests/DelegatePatternUsingProtocolTests.m
@@ -0,0 +1,32 @@
+//
+//  DelegatePatternUsingProtocolTests.m
+//  DelegatePatternUsingProtocolTests
+//
+//  Created by Daren David Taylor on 15/08/2013.
+//  Copyright (c) 2013 DarenDavidTaylor.com. All rights reserved.
+//
+
+#import "DelegatePatternUsingProtocolTests.h"
+
+@implementation DelegatePatternUsingProtocolTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DelegatePatternUsingProtocolTests");
+}
+
+@end
